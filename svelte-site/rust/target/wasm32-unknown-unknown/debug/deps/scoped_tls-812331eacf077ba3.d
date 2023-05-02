3c690d2c2a1d8cf889e985ef17e93640a9728c4f
Gray-lab <martin.grym@gmail.com>
2023-05-01 22:24:38 -0500

    svelte rust targets

---
+++ svelte-site/rust/target/wasm32-unknown-unknown/debug/deps/scoped_tls-812331eacf077ba3.d
@@ -0,0 +1,7 @@
+/home/sapphire/nand2tetris/svelte-site/rust/target/wasm32-unknown-unknown/debug/deps/scoped_tls-812331eacf077ba3.rmeta: /home/sapphire/.cargo/registry/src/github.com-1ecc6299db9ec823/scoped-tls-1.0.1/src/lib.rs
+
+/home/sapphire/nand2tetris/svelte-site/rust/target/wasm32-unknown-unknown/debug/deps/libscoped_tls-812331eacf077ba3.rlib: /home/sapphire/.cargo/registry/src/github.com-1ecc6299db9ec823/scoped-tls-1.0.1/src/lib.rs
+
+/home/sapphire/nand2tetris/svelte-site/rust/target/wasm32-unknown-unknown/debug/deps/scoped_tls-812331eacf077ba3.d: /home/sapphire/.cargo/registry/src/github.com-1ecc6299db9ec823/scoped-tls-1.0.1/src/lib.rs
+
+/home/sapphire/.cargo/registry/src/github.com-1ecc6299db9ec823/scoped-tls-1.0.1/src/lib.rs:
